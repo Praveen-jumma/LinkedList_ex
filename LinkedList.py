73049d0b3442c6dbcbefcbf6ed8133be5a3445bf
praveenjumma23@gmail.com <praveenjumma23@gmail.com>
2025-12-01 15:34:29 +0530

    added insert at end

--- LinkedList.py
+++ LinkedList.py
@@ -11,6 +11,14 @@ class LinkedList:
         nn = Node(data)
         nn.next = self.head
         self.head = nn
+    def insert_at_end(self,data):
+        nn = Node(data)
+        temp = self.head
+        while temp.next != None :
+          temp = temp.next
+        temp.next = nn
+
+
 
     def display(self):
         temp = self.head
@@ -23,4 +31,5 @@ L1 = LinkedList()
 L1.insert_at_first(12)
 L1.insert_at_first(14)
 L1.insert_at_first(16)
+L1.insert_at_end(20)
 L1.display()
