6acc23897142a03eaf4c4d87551251cba4423b8a
praveenjumma23@gmail.com <praveenjumma23@gmail.com>
2025-12-01 14:53:19 +0530

    first commit

---
+++ LinkedList.py
@@ -0,0 +1,26 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def insert_at_first(self, data):
+        nn = Node(data)
+        nn.next = self.head
+        self.head = nn
+
+    def display(self):
+        temp = self.head
+        while temp:
+            print(temp.data, end="-->")
+            temp = temp.next
+        print("None")
+
+L1 = LinkedList()
+L1.insert_at_first(12)
+L1.insert_at_first(14)
+L1.insert_at_first(16)
+L1.display()
